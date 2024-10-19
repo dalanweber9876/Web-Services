eebf690e274a738e7727d34309036f53776a6c2b
Dalan Weber <dalanweber9876@gmail.com>
2024-10-19 15:01:10 -0600

    Delete W05/Application-Tracker directory

--- W05/Application-Tracker/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/', require('./swagger'));
-router.use('/applications', require('./applications'));
-router.use('/companies', require('./companies'));
-
-module.exports = router;
