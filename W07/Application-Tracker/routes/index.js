6a5b64673c0c18cca3a08bf3f1c642251d1a22c6
dalanweber9876 <dalanweber9876@gmail.com>
2024-11-01 13:06:25 -0600

    Updated frontend to reflect all requirements, still missing functionality

--- W07/Application-Tracker/routes/index.js
+++ W07/Application-Tracker/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// This is to display the specific message based on if the user is authenticated.
 router.use('/', (req, res) => {
   const isAuthenticated = true; // Change to true or false to test
   res.render('../frontend/index.ejs', {
