3946d51d3bab73747bf3471c4db770dd1e2e92cc
dalanweber9876 <dalanweber9876@gmail.com>
2024-11-01 18:46:25 -0600

    Fixed path issue

--- W07/Application-Tracker/routes/index.js
+++ W07/Application-Tracker/routes/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./swagger'));
+router.use('/applications', require('./applications'));
+router.use('/companies', require('./companies'));
+
 // This is to display the specific message based on if the user is authenticated.
 router.use('/', (req, res) => {
-  const isAuthenticated = true; // Change to true or false to test
+  const isAuthenticated = false; // Change to true or false to test
   res.render('../frontend/index.ejs', {
     isAuthenticated: isAuthenticated
   });
 });
-router.use('/', require('./swagger'));
-router.use('/applications', require('./applications'));
-router.use('/companies', require('./companies'));
 
 module.exports = router;
