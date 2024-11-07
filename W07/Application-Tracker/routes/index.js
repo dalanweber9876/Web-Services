b7b57c8f654451eddfdf00e62e6f1bad476f7400
dalanweber9876 <dalanweber9876@gmail.com>
2024-11-06 23:14:52 -0700

    Removed unnecessary frontend

--- W07/Application-Tracker/routes/index.js
+++ W07/Application-Tracker/routes/index.js
@@ -5,12 +5,4 @@ router.use('/', require('./swagger'));
 router.use('/applications', require('./applications'));
 router.use('/companies', require('./companies'));
 
-// This is to display the specific message based on if the user is authenticated.
-router.use('/', (req, res) => {
-  const isAuthenticated = false; // Change to true or false to test
-  res.render('../frontend/index.ejs', {
-    isAuthenticated: isAuthenticated
-  });
-});
-
 module.exports = router;
